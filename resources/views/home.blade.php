28fab0a06611dd482f06b32e4da6be1dc19c2fd1
JorgeLuix <tec.jorge.29@gmai.com>
2023-05-23 14:56:32 +0200

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,6 @@
     <section class="container">
         <h1>Home page</h1>
         <p>Hello world</p>
-        {{-- <a href="{{ route('books.index') }}">Vai alla lista dei libri</a> --}}
+        {{-- <a href="{{ route('movies.index') }}">Vai alla lista dei movie</a> --}}
     </section>
 @endsection
